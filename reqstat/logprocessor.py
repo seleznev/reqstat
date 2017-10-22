9dcc8e259f3ea0f51ec2bc50f9047651b00c6fa1
Aleksandr Seleznev <alex@slznv.net>
2017-10-22 17:58:46 +0700

    Remove LogProcessor class

--- reqstat/logprocessor.py
+++ reqstat/logprocessor.py
@@ -23,71 +23,66 @@ LOG_FORMAT = re.compile(
     "\"(?P<http_referer>[^\"]*)\" \"(?P<http_user_agent>.*)\"$"
 )
 
-class LogProcessor():
+def get_key(status):
+    if status >= 200 and status <= 299:
+        return "2XX"
+    elif status >= 300 and status <= 399:
+        return "3XX"
+    elif status >= 400 and status < 499:
+        return "4XX"
+    elif status == 499:
+        return "499"
+    elif status >= 500 and status <= 599:
+        return "5XX"
+    else:
+        return "unknown"
 
-    @staticmethod
-    def get_key(status):
-        if status >= 200 and status <= 299:
-            return "2XX"
-        elif status >= 300 and status <= 399:
-            return "3XX"
-        elif status >= 400 and status < 499:
-            return "4XX"
-        elif status == 499:
-            return "499"
-        elif status >= 500 and status <= 599:
-            return "5XX"
-        else:
-            return "unknown"
-
-    @staticmethod
-    def parse_log_entry(line):
-        r = re.search(LOG_FORMAT, line)
-        if r:
-            return {
-                "status": int(r.group("status"))
-            }
-        else:
-            raise ValueError("parsing error: {}".format(line))
-
-    @staticmethod
-    def process_queue(queue, messages_type="syslog"):
-        stats = {
-            "2XX": 0,
-            "3XX": 0,
-            "4XX": 0,
-            "499": 0,
-            "5XX": 0,
-            "unknown": 0,
-            "total": 0
+def parse_log_entry(line):
+    r = re.search(LOG_FORMAT, line)
+    if r:
+        return {
+            "status": int(r.group("status"))
         }
+    else:
+        raise ValueError("parsing error: {}".format(line))
 
-        while True:
-            item = queue.get()
+def process_queue(queue, messages_type="syslog"):
+    stats = {
+        "2XX": 0,
+        "3XX": 0,
+        "4XX": 0,
+        "499": 0,
+        "5XX": 0,
+        "unknown": 0,
+        "total": 0
+    }
 
-            if item is None:
-                break
+    while True:
+        item = queue.get()
 
-            try:
-                if messages_type == "syslog":
-                    r = re.search(re.compile("^\<[\d]+\>[\w]+ [\d]+ \d\d:\d\d:\d\d [\w]+ [\w]+: (?P<line>.*)$"),
-                                  item.decode("utf-8"))
-                    if r:
-                        line = r.group("line")
-                    else:
-                        raise ValueError("parsing error: {}".format(item))
+        if item is None:
+            break
+
+        try:
+            if messages_type == "syslog":
+                r = re.search(re.compile("^\<[\d]+\>[\w]+ [\d]+ \d\d:\d\d:\d\d [\w]+ [\w]+: (?P<line>.*)$"),
+                              item.decode("utf-8"))
+                if r:
+                    line = r.group("line")
                 else:
-                    line = item
+                    raise ValueError("parsing error: {}".format(item))
+            else:
+                line = item
 
-                result = LogProcessor.parse_log_entry(line)
-            except ValueError as e:
-                #print_error(str(e))
-                break
+            result = parse_log_entry(line)
+        except ValueError as e:
+            #print_error(str(e))
+            break
 
-            stats[LogProcessor.get_key(result["status"])] += 1
-            stats["total"] += 1
+        stats[get_key(result["status"])] += 1
+        stats["total"] += 1
 
-            queue.task_done()
+        queue.task_done()
 
-        return stats;
+    return stats;
 
